4ebe59138e566c8b46343add7c75ada61fd69a6d
joyal <joyal.joy@my.jcu.edu.au>
2024-11-03 20:19:37 +1000

    Complete prac 6 code

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -20,7 +20,6 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-    print("\n... snip ...\n")
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
