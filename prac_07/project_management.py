02c882a1be20bc0dbaf3d13dd625c4c3ec826894
joyal <joyal.joy@my.jcu.edu.au>
2024-11-09 16:09:03 +1000

    fixed valid choice in project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -41,9 +41,10 @@ def display_menu():
 
 
 def get_valid_choice():
-    """ Prompts for a valid menu choice"""
+    """Prompts for a valid menu choice"""
+    valid_choices = ('L', 'S', 'D', 'F', 'A', 'U', 'Q')
     user_choice = input(">>> ").upper()
-    while user_choice != 'L,S,D,F,A,U,Q':
+    while user_choice not in valid_choices:
         print("Invalid choice. Please try again.")
         user_choice = input(">>> ").upper()
     return user_choice
